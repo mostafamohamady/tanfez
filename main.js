ad47a6c212cd028a43525ef386aef04e2403d224
Mostafa <a.sakr@cch-kuwait.com>
2024-09-16 19:32:17 +0300

    update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let startBtn = document.querySelector("#startBtn");
 
 startBtn.addEventListener("click", function () {
-  console.log("Star");
+
   startBtn.style = "display:none";
 
   gsap.to(".behind-logo", {
@@ -11,34 +11,48 @@ startBtn.addEventListener("click", function () {
     ease: "myBounce",
   });
   gsap.to(".logo", { scale: 2, duration: 0.7, delay: 0.2 });
-  gsap.to(".logo", { y: "12vh", duration: 0.5, delay: 1 });
+  gsap.to(".logo", { y: "72px", duration: 0.5, delay: 1 });
   gsap.to(".logo1", {
     display: "block",
-    height: "150px",
+  
+    
     duration: 0.5,
     delay: 1.5,
+    scale: 1
   });
   gsap.to(".logo2", {
     display: "block",
-    height: "150px",
+    
     duration: 0.5,
     delay: 2,
+    scale: 1
   });
   gsap.to(".logo3", {
     display: "block",
-    height: "150px",
+    
     duration: 0.5,
     delay: 2.5,
+    scale: 1
   });
+  gsap.to(".leftanimation", {
+    x:"25vw",
+    
+    duration: 0.8,
+    delay: 3,
+    
+  });
+
+
   gsap.to(".logo-container", {
     y: "-10vh",
     scale: 0.8,
     duration: 0.8,
     delay: 3,
+    scale: 1
   });
   gsap.to(".tanfiz-title", {
     display: "block",
-    y: "-15vh",
+    y: "-20vh",
     scale: 1.2,
     duration: 0.8,
     delay: 3.5,
@@ -51,14 +65,14 @@ startBtn.addEventListener("click", function () {
   });
   gsap.to(".tanfiz-title", {
     display: "block",
-    y: "-45vh",
+    y: "-55vh",
     scale: 0.9,
     duration: 0.8,
     delay: 4.5,
   });
   gsap.to(".login-form", {
     display: "block",
-    y: "-45vh",
+    y: "-55vh",
     scale: 0.9,
     duration: 0.8,
     delay: 4.5,
